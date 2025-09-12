63c960bb3feca340b297e511b0192793de5b2354
Disha Naveen <nnm24is075@nmamit.in>
2025-09-12 17:35:01 +0530

    Add files via upload

---
+++ Task1/Issueable.java
@@ -0,0 +1,11 @@
+package oopTask1;
+
+public interface Issueable
+{
+	
+	    void bookIssue(String Person) throws Exception;
+	    void returningTheBook() throws Exception;
+	    boolean isAvailable();
+	}
+
+
