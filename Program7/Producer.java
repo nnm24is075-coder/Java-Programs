8220f27463810b23ecdf36afdd2382393c2d20b3
Disha Naveen <nnm24is075@nmamit.in>
2025-10-06 21:28:35 +0530

    Add files via upload

---
+++ Program7/Producer.java
@@ -0,0 +1,30 @@
+package message;
+
+
+class Producer extends Thread {
+    private MessageBoard board;
+
+    public Producer(MessageBoard b) {
+        this.board = b;
+    }
+
+    @Override
+    public void run() {
+        String msgs[] = {
+            "Exam on Monday",
+            "Holiday on Tuesday",
+            "Workshop on Wednesday"
+        };
+
+        for (String msg : msgs) {
+            board.put(msg);
+            try {
+                Thread.sleep(1000); // simulate delay
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        }
+        board.put("DONE"); // signal end of messages
+    }
+}
+
